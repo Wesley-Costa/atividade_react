4efab72e9cc166b25aa9cdba946fdae13172d559
Wesley Santos Costa <wesleycosta06@hotmail.com>
2021-10-20 19:56:49 -0300

    Fase 1: atividade da primeira aula de react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './pages/Header/index';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header title = "User App - Deu Bom"/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
